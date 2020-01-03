c28122e7bcd1b497edc64f968324045547ba43c9
guillsav <guillsav@gmail.com>
2020-01-02 22:58:11 -0500

    Coffeemakers routes done

--- ressources/coffemakers/coffemakers-route.js
+++ ressources/coffemakers/coffemakers-route.js
@@ -12,6 +12,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const coffeemaker = await db.findById(req.params.id);
+        if (!coffeemaker)
+            res.status(404).json({
+                errorMessage: `Coffeemaker with ID ${req.params.id} could not be found!`
+            });
+        res.status(200).json(coffeemaker);
+    } catch ({message}) {
+        res.status(500).json({
+            errorMessage: `Server could not retrieve coffeemaker.`
+        });
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const newCoffeemaker = await db.insert(req.body);
@@ -19,6 +34,36 @@ router.post('/', async (req, res) => {
     } catch ({message}) {
         res.status(500).json({
             errorMessage: `Could not add a new coffeemaker to the database.`
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const coffeemaker = await db.update(req.params.id, req.body);
+        if (!coffeemaker)
+            res.status(404).json({
+                errorMessage: `Coffeemaker with ID ${req.params.id} could not be found!`
+            });
+        res.status(201).json(coffeemaker);
+    } catch ({message}) {
+        res.status(500).json({
+            errorMessage: `Could not retrieve coffeemaker from the database.`
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const coffeemaker = await db.remove(req.params.id);
+        if (!coffeemaker)
+            res.status(404).json({
+                errorMessage: `Coffeemaker with ID ${req.params.id} could not be found!`
+            });
+        res.status(204).end();
+    } catch ({message}) {
+        res.status(500).json({
+            errorMessage: `Could not delete the coffeemaker from the database.`
         });
     }
 });
